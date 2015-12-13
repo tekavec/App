4968fdd72050aa802ea0a7e5c4711b2beb1718ba
Tomaž Tekavec <tomaz@codurance.com>
2015-12-13 17:47:23 +0000

    Initial solution.

---
+++ App/CustomerCreditService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App
+{
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    [System.ServiceModel.ServiceContractAttribute(ConfigurationName = "App.ICustomerCreditService")]
+    public interface ICustomerCreditService
+    {
+
+        [System.ServiceModel.OperationContractAttribute(Action = "http://tempuri.org/ICustomerCreditService/GetCreditLimit", ReplyAction = "http://tempuri.org/ICustomerCreditService/GetCreditLimitResponse")]
+        int GetCreditLimit(string firstname, string surname, System.DateTime dateOfBirth);
+    }
+
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    public interface ICustomerCreditServiceChannel : App.ICustomerCreditService, System.ServiceModel.IClientChannel
+    {
+    }
+
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    public partial class CustomerCreditServiceClient : System.ServiceModel.ClientBase<App.ICustomerCreditService>, App.ICustomerCreditService
+    {
+
+        public CustomerCreditServiceClient()
+        {
+        }
+
+        public CustomerCreditServiceClient(string endpointConfigurationName) :
+            base(endpointConfigurationName)
+        {
+        }
+
+        public CustomerCreditServiceClient(string endpointConfigurationName, string remoteAddress) :
+            base(endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public CustomerCreditServiceClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) :
+            base(endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public CustomerCreditServiceClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) :
+            base(binding, remoteAddress)
+        {
+        }
+
+        public int GetCreditLimit(string firstname, string surname, System.DateTime dateOfBirth)
+        {
+            return base.Channel.GetCreditLimit(firstname, surname, dateOfBirth);
+        }
+    }
+}
