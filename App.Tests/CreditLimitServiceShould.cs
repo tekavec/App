3ec8c79f5be01261a3d48a0e17b80248fba3be27
Tomaž Tekavec <tomaz@codurance.com>
2015-12-14 00:52:39 +0000

    Add test for CreditLimitService for VeryImportantClient.

---
+++ App.Tests/CreditLimitServiceShould.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace App.Tests
+{
+    [TestFixture]
+    public class CreditLimitServiceShould
+    {
+        private string _VeryImportantClientName = "VeryImportantClient";
+        private bool _HasCreditLimit = true;
+        private bool _HasNoCreditLimit = false;
+
+        [Test]
+        public void set_unlimited_credit_limit_to_customer_for_very_important_client()
+        {
+            var customer = new Customer {HasCreditLimit = _HasCreditLimit, Company = new Company {Name = _VeryImportantClientName } };
+            var creditLimitService = new CreditLimitService();
+            
+            creditLimitService.SetCreditLimitTo(customer);
+
+            Assert.IsFalse(customer.HasCreditLimit);
+            
+        }
+
+    }
+}
